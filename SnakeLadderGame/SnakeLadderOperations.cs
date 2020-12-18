f92f1af541324bb52a78d6290b09abe2bb8adb39
abh224272 <abh224272@gmail.com>
2020-12-18 10:50:29 +0530

    [Abhishek Mishra] Add: Method to Roll the Dice

---
+++ SnakeLadderGame/SnakeLadderOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderGame
+{
+    class SnakeLadderOperations
+    {
+        public int STARTING_POSITION = 0;
+        Random random = new Random();
+
+        public void DiceRoll()
+        {
+            int dice = (random.Next(6) + 1);
+            Console.WriteLine("Dice Number  " + dice);
+        }
+    }
+}
