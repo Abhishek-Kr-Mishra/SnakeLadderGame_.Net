98c4028304b91c6a6d3638b0d2386deb26023985
abh224272 <abh224272@gmail.com>
2020-12-18 10:29:53 +0530

    [Abhishek Mishra] Add: Initialized Starting Position for the Game

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -4,9 +4,11 @@ namespace SnakeLadderGame
 {
     class Program
     {
+        public static int STARTING_POSITION = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            Console.WriteLine("Starting Position " + STARTING_POSITION);
         }
     }
 }
